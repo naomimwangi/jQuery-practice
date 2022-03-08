2618d7423e3fe5d091ec24822e60de6343a06961
naomimwangi <naomimwangi7@gmail.com>
2022-03-08 17:31:30 +0300

    add js elements

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(h1).click (function (){
+    alert ('This is a heading function')
+
+});
+$(p).click (function (){
+    alert ('This is a paragraph')
+});
+$(img).click (function (){
+    alert (' This is an image')
+});
+
